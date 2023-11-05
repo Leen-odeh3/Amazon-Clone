f41d58d1701d28e01619c0337a6557fe8194f507
leen <leenodeh287@gmail.com>
2023-11-05 16:32:34 +0300

    add all assets

--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -4,9 +4,18 @@ import './Header.scss'
 const Header = () => {
   return (
   <header>
+<div>
+  <img src="/login-logo.png" alt="amazon-logo"/>  
+</div>
+
+<div>
+
+</div>
+<div>
 
 
-    
+</div>
+
   </header>
 
   )
