805a14123b4ea43f96246f7907b7d7192ad99b29
leen <leenodeh287@gmail.com>
2023-11-05 16:26:54 +0300

    update routing

--- src/Routing/index.jsx
+++ src/Routing/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Login from '../Forms/Login/Login'
+import Signup from '../Forms/Signup/signup'
+import Header from '../component/Header/Header'
 
 const index = () => {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
         <Route path="/Login" element={<Login/>}/>
+        <Route path="/signup" element={<Signup/>}/>
     </Routes>
     </BrowserRouter>
   )
---
+++ src/component/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.scss'
+
+const Header = () => {
+  return (
+  <header>
+
+
+    
+  </header>
+
+  )
+}
+
+export default Header
