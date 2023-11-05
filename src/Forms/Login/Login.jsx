286975a3910d95ae40f66c06cf327dc9a5dc7e78
leen <leenodeh287@gmail.com>
2023-11-05 19:44:05 +0300

    SignUp page done

--- src/Forms/Login/Login.jsx
+++ src/Forms/Login/Login.jsx
@@ -1,43 +1,44 @@
 import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
-import './Login.scss'
+import { Link, useNavigate } from "react-router-dom";
+import "./Login.scss";
 
 const Login = ({ show }) => {
   useEffect(() => {
     show(false);
   }, []);
 
-  return  <div className="login">
-  <Link to="/">
-    <img className="login-logo" src="/login-logo.png" alt="logo-img" />
-  </Link>
-  <div className="login-container">
-    <h1>Sign in</h1>
-    <form>
-      <h5>Email</h5>
-      <input
-        type="email"
-       
-      />
-      <h5>Password</h5>
-      <input
-        type="password"
-        
-        
-      />
-      <button className="login-signInBtn" type="submit">
-        Sign in
-      </button>
-      <p>
-        By continuing, you agree to Amazon's Fake Clone Conditions of Use
-        and Privacy Notice.
-      </p>
-      <button className="login-registerBtn" >
-        Create your Amazon Account
-      </button>
-    </form>
-  </div>
-</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="login">
+      <Link to="/">
+        <img className="login-logo" src="/login-logo.png" alt="logo-img" />
+      </Link>
+      <div className="login-container">
+        <h1>Sign in</h1>
+        <form>
+         
+          <h5>Email</h5>
+          <input type="email" />
+          <h5>Password</h5>
+          <input type="password" />
+          <button className="login-signInBtn" type="submit">
+            Sign in
+          </button>
+          <p>
+            By continuing, you agree to Amazon's Fake Clone Conditions of Use
+            and Privacy Notice.
+          </p>
+          <button
+            className="login-registerBtn"
+            onClick={() => navigate("/signup")}
+          >
+            Create your Amazon Account
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/Forms/Signup/signup.jsx
+++ src/Forms/Signup/signup.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "./signup.scss";
 
 const signup = ({ show }) => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -6,7 +8,39 @@ const signup = ({ show }) => {
     show(false);
   }, []);
 
-  return <div>signup</div>;
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const navigate= useNavigate();
+  return (
+    <div className="login">
+      <Link to="/">
+        <img className="login-logo" src="/login-logo.png" alt="logo-img" />
+      </Link>
+      <div className="login-container">
+        <h1>Create account</h1>
+        <form>
+          <h5> Your Name</h5>
+          <input type="text" />
+          <h5>Email</h5>
+          <input type="email" />
+          <h5>Password</h5>
+          <input type="password" />
+          <h5> Re-enter password</h5>
+          <input type="text" />
+          <button className="login-signInBtn" type="submit">
+            Sign up
+          </button>
+          <p>
+            By continuing, you agree to Amazon's Fake Clone Conditions of Use
+            and Privacy Notice.
+          </p>
+          <div className="cat-btn">
+            <p className="cat-div">Already have an account?</p>
+          </div>
+          <button className="login-registerBtn" onClick={()=> navigate("/login")}>Sign in</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default signup;
