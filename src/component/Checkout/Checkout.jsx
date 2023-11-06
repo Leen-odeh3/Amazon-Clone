ae39e3cf8d0d8f0716520a2dc9a380e9c40046ae
leen <leenodeh287@gmail.com>
2023-11-06 14:42:38 +0300

    Checkout Page

---
+++ src/component/Checkout/Checkout.jsx
@@ -0,0 +1,45 @@
+
+import React from "react";
+import { auth } from "./../../Firebase/config.js";
+import { useAuthState } from "react-firebase-hooks/auth";
+import CheckoutProduct from "./CheckoutProduct";
+import "./Checkout.scss";
+import {basket} from './../../data/cart.js'
+// import Subtotal from "./Subtotal";
+
+const Checkout = () => {
+    const [user] = useAuthState(auth);
+  return (
+    <div className="checkout">
+      <div className="checkout-left">
+        <img className="checkout-ad" src="/checkoutAd.jpg" alt=""/>
+        <div>
+          <h3>Hello, {user?.email}</h3>
+          <h2 className="checkout-title">Your shopping Basket</h2>
+          {basket.length > 0 ? (
+            basket.map((item) => (
+              <CheckoutProduct
+                key={item.id}
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))
+          ) : (
+            <p>
+              You have no items in your basket.To buy one or more
+              items,click"Add to basket".
+            </p>
+          )}
+        </div>
+      </div>
+      <div className="checkout-right">
+        {/* <Subtotal /> */}
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
---
+++ src/data/cart.js
@@ -0,0 +1,19 @@
+export const basket = [
+  {
+    id: 1,
+    image:
+      "https://res.cloudinary.com/dll0c6zey/image/upload/v1699270752/2.0f5bc477d58244e1dbf1_fitslx.png",
+    price: 682.95,
+    title: "Lenovo - 2021 - IdeaPad 3 - Gaming Laptop",
+    rating: 4,
+  },
+  {
+    id: 2,
+    image:
+      "https://res.cloudinary.com/dll0c6zey/image/upload/v1699270752/1.f039a27703de29c4ec4c_gj0joi.png",
+    price: 64,
+    title:
+      "Razer Kraken Tournament Edition THX 7.1 Surround Sound Gaming Headset",
+    rating: 2,
+  },
+];
